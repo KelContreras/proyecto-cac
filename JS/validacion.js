e238d093c8434295d6ec62741c921c891f2394a0
Belen <maruferrari988@gmail.com>
2023-05-13 20:11:12 -0300

    validacion formulario

---
+++ JS/validacion.js
@@ -0,0 +1,71 @@
+var errorNombre = document.getElementById('error-nombre');
+var errorEmail = document.getElementById('error-email');
+var errorNumero = document.getElementById('error-number');
+var errorMensaje = document.getElementById('error-mensaje');
+var errorSubmit= document.getElementById('error-submit');
+
+function validacionName(){
+    var nombre = document.getElementById('contacto-name').value;
+
+    if (nombre.length == 0){
+        errorNombre.innerHTML= 'Completar nombre';
+        return false;
+    }
+    if (!nombre.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+        errorNombre.innerHTML= 'Escribe el nombre completo';
+        return false;
+    }
+    errorNombre.innerHTML= "Valido";
+    return true;
+}
+function validacionEmail(){
+    var email = document.getElementById('contacto-email').value;
+
+    if (email.length == 0){
+        errorEmail.innerHTML= 'Completar email';
+        return false;
+    }
+    if (!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+        errorEmail.innerHTML= 'Email invalido';
+        return false;
+    }
+    errorEmail.innerHTML= "Valido";
+    return true;
+}
+function validacionNumero(){
+    var numero = document.getElementById('contacto-number').value;
+
+    if (numero.length == 0){
+        errorNumero.innerHTML= 'Completar telefono';
+        return false;
+    }
+    if (numero.length !== 10){
+        errorNumero.innerHTML= 'Debe tener 10 digitos';
+        return false;
+    }
+    if (!numero.match(/^[0-9]{10}$/)){
+        errorNumero.innerHTML= 'Solo digitos';
+        return false;
+    }
+    errorNumero.innerHTML= "Valido";
+    return true;
+}
+function validacionMensaje(){
+    var mensaje = document.getElementById('contacto-mensaje').value;
+    var cantidad = 10;
+    var c = cantidad - mensaje.length;
+
+    if ( c > 0){
+    errorMensaje.innerHTML= c + 'Se necesitan al menos 10 caracteres';
+    return false;
+    }
+    errorMensaje.innerHTML= "Valido";
+    return true;
+    }
+
+function validacionFormulario(){
+    if (!validacionName() || !validacionEmail() || !validacionNumero() || !validacionMensaje()){
+        errorSubmit.innerHTML="Los valores son incorrectos";
+
+    }
+}
