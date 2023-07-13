0f6814bf2b3f2ee82e8c6299778ce91404c22bc8
KelContreras~ <kelvinjcontrerasm@gmail.com>
2023-07-12 23:51:21 -0300

    agregue login, comunicacion con el backend

---
+++ JS/instrumentos.js
@@ -0,0 +1,72 @@
+const { createApp } = Vue
+  createApp({
+    data() {
+      return {
+        instrumentos:[],
+        //url:'http://localhost:5000/productos', 
+   // si el backend esta corriendo local  usar localhost 5000(si no lo subieron a pythonanywhere)
+        url:'https://kelvinjcontrerasm.pythonanywhere.com/instrumentos',   // si ya lo subieron a pythonanywhere
+        error:false,
+        cargando:true,
+        /*atributos para el guardar los valores del formulario */
+
+        
+        id:0,
+        marca :"", 
+        nombre :"",
+        precio :0,
+    }  
+    },
+    methods: {
+        fetchData(url) {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    this.productos = data;
+                    this.cargando=false
+                })
+                .catch(err => {
+                    console.error(err);
+                    this.error=true              
+                })
+        },
+        eliminar(id) {
+            const url = this.url+'/' + id;
+            var options = {
+                method: 'DELETE',
+            }
+            fetch(url, options)
+                .then(res => res.text()) // or res.json()
+                .then(res => {
+			 alert('Registro Eliminado')
+                    location.reload(); // recarga el json luego de eliminado el registro
+                })
+        },
+        grabar(){
+            let instrumento = {
+                marca:this.marca,
+                nombre:this.nombre,
+                precio:this.precio,
+                
+            }
+            var options = {
+                body:JSON.stringify(instrumento),
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                redirect: 'follow'
+            }
+            fetch(this.url, options)
+                .then(function () {
+                    alert("Registro grabado")
+                    window.location.href = "./instrumentos.html";  // recarga instrumentos.html
+                })
+                .catch(err => {
+                    console.error(err);
+                    alert("Error al Grabar")  // puedo mostrar el error tambien
+                })      
+        }
+    },
+    created() {
+        this.fetchData(this.url)
+    },
+  }).mount('#app')
--- JS/main.js
+++ JS/main.js
@@ -9,33 +9,3 @@ document.querySelector("button.menu-toggle")
                       classList.toggle("show")})
 
 
-
-const { createApp } = Vue;
-
-createApp({
-  data() {
-    return {
-      artistInfo: null
-    };
-  },
-  mounted() {
-    this.getArtistInfo('Nombre del artista');
-  },
-  methods: {
-    getArtistInfo(artistName) {
-      const API_KEY = '702a6656c7e67685f795383b6a137bf4';
-      const url = `http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artistName}&api_key=${API_KEY}&format=json`;
-
-      fetch(url)
-        .then(response => response.json())
-        .then(data => {
-          this.artistInfo = data.artist;
-          console.log(this.artistInfo);
-        })
-        .catch(error => {
-          console.error(error);
-        });
-    }
-  }
-}).mount('#app');
-
